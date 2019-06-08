2636eae854833469f4ca38fde0ce9ecabc5c5034
bmurugan <bala.ramnad@gmail.com>
2019-06-08 08:00:32 -0700

    Update init.el

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
             '("marmalade" . "https://marmalade-repo.org/packages/") t)
 
 (defvar my-packages '(auctex solarized-theme ido-ubiquitous idle-highlight-mode paredit better-defaults))
-    
+
 (package-initialize)
   (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -54,25 +54,3 @@
 ;;(setq eshell-where-to-jump 'begin)
 ;;(setq eshell-review-quick-commands nil)
 ;;(setq eshell-smart-space-goes-to-end t)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(fset 'float-includegraphics
-   [?\C-s ?^ ?\\ ?s ?- ?* ?\\ ?\\ ?i ?n ?c ?l ?u ?d ?e ?g ?r ?a ?p ?h return ?\C-a ?\\ ?b ?e ?g ?i ?n ?\{ ?f ?i ?g ?u ?r ?e ?\} ?\[ ?h ?t ?b ?p ?! ?\] return tab ?\\ ?c ?e ?n ?t ?e ?r ?i ?n ?g return tab ?\C-e return ?\\ ?e ?n ?d ?\{ ?f ?i ?g ?u ?r ?e ?\} tab return ?\C-k backspace return up ?\C-e ?\C-x ?\C-s])
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default))
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(solarized-theme px paredit latex-preview-pane ido-ubiquitous idle-highlight-mode idle-highlight better-defaults auctex))
- '(safe-local-variable-values '((TeX-master . t))))
